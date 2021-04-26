957647a6ffe44069027b6e1ebc91c1075747deca
Alec Tecson <tecsonalr@gmail.com>
2021-04-26 19:02:19 +0800

    Classes

---
+++ Classes/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introducte()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using Classes.Math;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introducte();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            System.Console.WriteLine(result);
+        }
+    }
+}
