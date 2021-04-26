6367cb95fcf598593fb00b7a460a48671e30c7af
Alec Tecson <tecsonalr@gmail.com>
2021-04-26 20:54:04 +0800

    Strings

---
+++ Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Mosh";
+            var lastName = "Hamedani";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] {"John", "Jack", "Mary"};
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            //var text = "Hi John\nLook into the following paths \nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            var text = @"Hi John
+Look into the following paths 
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
+        }
+    }
+}
