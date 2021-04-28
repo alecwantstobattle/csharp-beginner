674b6e868728dae6d25ba95dd7e581802d4f2685
Alec Tecson <tecsonalr@gmail.com>
2021-04-28 23:46:31 +0800

    Arrays - Length Property and Methods

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -10,21 +10,42 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            var numbers = new int[3];
-            numbers[0] = 1;
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
 
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
+            // Length
+            Console.WriteLine("Length: " + numbers.Length);
 
-            var flags = new bool[3];
-            flags[0] = true;
+            // IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: " + index);
 
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
+            // Clear()
+            Array.Clear(numbers, 0, 2);
 
-            var names = new string[3] {"Jack", "John", "Mary"};
+            foreach(var n in numbers)
+                Console.WriteLine(n);
+
+            // Copy()
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+
+            Console.WriteLine("Effect of Copy()");
+            foreach(var n in another)
+                Console.WriteLine(n);
+
+            // Sort()
+            Array.Sort(numbers);
+
+            Console.WriteLine("Effect of Sort()");
+            foreach(var n in numbers)
+                Console.WriteLine(n);
+
+            // Reverse
+            Array.Reverse(numbers);
+
+            Console.WriteLine("Effect of Reverse()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
         }
     }
 }
