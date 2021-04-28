2e21dc9a295e1a530c077e57930316fb8e516c1c
Alec Tecson <tecsonalr@gmail.com>
2021-04-28 20:07:48 +0800

    WhileLoop

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (var i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+            //}
+
+            //var i = 0;
+            //while(i <= 10)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+
+            //    i++;
+            //}    
+
+            //while(true)
+            //{
+            //    Console.Write("Type your name: ");
+            //    var input = Console.ReadLine();
+
+            //    if (String.IsNullOrWhiteSpace(input))
+            //        break;
+
+            //    Console.WriteLine("@Echo: " + input);
+            //}
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+                
+                break;                
+            }
+        }
+    }
+}
